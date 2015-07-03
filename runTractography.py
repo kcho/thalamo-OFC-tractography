2fb158cd1f44f7ee78bff88a8f4d4e8ce845d228
kevinMacPro <cke8671@hotmail.com>
2015-07-03 09:55:28 +0900

    new working version

--- runTractography.py
+++ runTractography.py
@@ -1,69 +1,93 @@
-#!/ccnc_bin/venv/bin/python
-
 import os
+import re
 import argparse
 import textwrap
 
-'''
-It runs probabilistic tractography using probtrackx2 in FSL.
-In the script, 
-    - the seed and the target ROI have been set as
-        - thalamus ROI
-        - OFC ROI
-    - using network option (obtaining the tract between seed and target)
-    - in T1 space (Therefore, the transformation matrix is used as well.)
-
-The script assumes that the 'subject' is in 
-    - '/ccnc/chrconThalamus' for CHR
-    - '/ccnc/chrconThalamus/SPD' for FEP
-
-and also assumes that DTI bedpostx have been completed in each subject as
-    - {subject}/DTI.bedpostX
-
-Future updates for more general usage
-    - Direct inputs from the shell
-        - ROIs
-        - Transformation matrix
-        - data location
-'''
-
-
 def runTractography(subject,subjectLocation,outDir,side):
+
+    # Location set
+    exclusionROI = os.path.join(outDir,'brain_stem.nii.gz')
+    terminationROI = os.path.join(outDir,'stop_mask.nii.gz')
+    roi_loc = os.path.join(subjectLocation,'ROI')
+    thalamus_ROI = os.path.join(roi_loc,
+                                '{0}_thalamus.nii.gz'.format(side))
+    OFC_ROI = os.path.join(roi_loc,
+                           '{0}_OFC.nii.gz'.format(side))
+    mask = '{thalamus_ROI_Location}\n{OFC_ROI_Location}\n'.format(
+                thalamus_ROI_Location = thalamus_ROI,
+                OFC_ROI_Location = OFC_ROI)
+
+
+
+    # Make output directory
     try:
         os.mkdir(outDir)
     except:
         pass
     
-    mask = '{thalamus_ROI_Location}\n{OFC_ROI_Location}\n'.format(
-                thalamus_ROI_Location =os.path.join(subjectLocation,'ROI','{0}_thalamus.nii.gz'.format(side)),
-                OFC_ROI_Location = os.path.join(subjectLocation,'ROI','{0}_OFC.nii.gz'.format(side)))
+
+    # Make exclusion ROI : brain stem ROI
+    brain_stem_extraction_command = 'mri_binarize \
+            --i {aseg} \
+            --match 16 \
+            --o {outLoc}'.format(
+                aseg = os.path.join(subjectLocation,
+                                    'freesurfer',
+                                    'mri',
+                                    'aseg.mgz'),
+                outLoc = exclusionROI)
+
+    brain_stem_extraction_command = re.sub('\s+',' ',brain_stem_extraction_command)
+    print brain_stem_extraction_command
+    print os.popen(brain_stem_extraction_command).read()
     
+
+    #Make ROI list as a text file : probtrackx2 input
     with open(os.path.join(outDir,'masks.txt'),'w') as f:
         f.write(mask)
     
+
+    
+    # Make termination ROI : LPFC + MPFC + LTC + MTC + PC + OCC + SMC
+    merge_list = ['LPFC','MPFC','LTC','MTC','PC','OCC','SMC']
+    merge_list_loc = [os.path.join(roi_loc,
+                                   side+'_'+x+'.nii.gz') for x in merge_list]
+    roi_merge_command = 'fslmaths {firstImg} {addList} {output}'.format(
+            firstImg = merge_list_loc[0],
+            addList = ' '.join(['-add '+x for x in merge_list_loc[1:]]),
+            output = terminationROI)
+
+    print os.popen(roi_merge_command).read()
+
+
+    
     
     command = '/usr/local/fsl/bin/probtrackx2 \
---network \
--x {mask} \
--l \
---onewaycondition \
--c 0.2 \
--S 2000 \
---steplength=0.5 \
--P 5000 \
---fibthresh=0.01 \
---distthresh=0.0 \
---sampvox=0.0 \
---xfm={subjectLocation}/registration/freesurferT1toNodif.mat \
---forcedir \
---opd \
--s {subjectLocation}/DTI.bedpostX/merged \
--m {subjectLocation}/DTI.bedpostX/nodif_brain_mask \
---dir={outDir}'.format(mask = os.path.join(outDir,'masks.txt'),
-                       subjectLocation = subjectLocation,
-                       outDir = outDir)
-    print command
-    print os.popen(command).read()
+                --network \
+                -x {mask} \
+                -l \
+                --onewaycondition \
+                -c 0.2 \
+                -S 2000 \
+                --steplength=0.5 \
+                -P 5000 \
+                --fibthresh=0.01 \
+                --distthresh=0.0 \
+                --sampvox=0.0 \
+                --xfm={subjectLocation}/registration/freesurferT1toNodif.mat \
+                --avoid={exclusionROI} \
+                --stop={terminationROI} \
+                --forcedir \
+                --opd \
+                -s {subjectLocation}/DTI.bedpostX/merged \
+                -m {subjectLocation}/DTI.bedpostX/nodif_brain_mask \
+                --dir={outDir}'.format(mask = os.path.join(outDir,'masks.txt'),
+                                       exclusionROI = exclusionROI,
+                                       terminationROI = terminationROI,
+                                       subjectLocation = subjectLocation,
+                                       outDir = outDir)
+    probtrackx2_command = re.sub('\s+',' ',command)  
+    print os.popen(probtrackx2_command).read()
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(
@@ -73,11 +97,10 @@ if __name__=='__main__':
             ========================================
             eg) runTractography -s CHR02_JHJ -o /Users/admin/Desktop/prac_4_python
             '''.format(codeName=os.path.basename(__file__))))
-
     parser.add_argument(
         '-s', '--subject',
-        help='name of the subject directory')
-        
+        help='name of the subject directory',
+        default=os.getcwd())
 
     parser.add_argument(
         '-o', '--out',
@@ -95,9 +118,6 @@ if __name__=='__main__':
     args = parser.parse_args()
 
 
-    if args.subject == None: 
-        parser.error('Please choose a subject')
-
     if args.left and args.right:
         parser.error('Please choose only one side')
     elif args.left:
@@ -105,13 +125,7 @@ if __name__=='__main__':
     else:
         side = 'rh'
 
-    if args.subject.startswith('FEP'):
-        runTractography(args.subject,
-                os.path.join('/ccnc/chrconThalamus/SPD',args.subject),
-                args.out,
-                side)
-    else:
-        runTractography(args.subject,
-                os.path.join('/ccnc/chrconThalamus',args.subject),
-                args.out,
-                side)
+    runTractography(args.subject,
+            os.path.join('/ccnc/chrconThalamus',args.subject),
+            args.out,
+            side)
